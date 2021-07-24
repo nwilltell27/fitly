3c8dcc5917b5309edb0e51c3addd3854b974e66d
Julia Vavrinyuk <juliav@Julias-MacBook-Pro.local>
2021-07-24 15:46:16 -0700

    created superuser and profie form

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('user/signup/', views.signup, name='signup'),
-  path('profile/', views.profile, name='profile'),
+  path('profile/<int:user_id>/', views.profile, name='profile'),
   path('profile/<int:user_id>/profile_form/', views.profile_form, name='profile_form'),
 ] 
