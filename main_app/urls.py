9773e33672e4bea4d9b6e78ea778c7acc32f8b40
Julia Vavrinyuk <juliav@Julias-MacBook-Pro.local>
2021-07-24 14:05:00 -0700

    commit all

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('user/signup/', views.signup, name='signup'),
   path('profile/', views.profile, name='profile'),
+  path('profile/<int:user_id>/profile_form/', views.profile_form, name='profile_form'),
 ] 
