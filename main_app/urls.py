0488ed374a7c5df2145ee80e603b4ff573311dc2
jimpfred <jimpfred@yahoo.com>
2021-07-24 18:53:41 -0700

    add elog crud

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name="home")
+  path('', views.home, name="home"),
+  path('elogs/', views.elogs_index, name="index"),
+  path('elogs/<int:user_id>/', views.elogs_detail, name='detail'),
+  path('elogs/<int:pk>/create/', views.ElogCreate.as_view(), name='elogs_create'),
+  path('elogs/<int:pk>/update/', views.ElogUpdate.as_view(), name='elogs_update'),
+  path('elogs/<int:pk>/delete/', views.ElogDelete.as_view(), name='elogs_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Elog, UserProfile
+from django.urls import reverse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+class ElogCreate(CreateView):
+  model = Elog
+  fields = '__all__'
+
+class ElogUpdate(UpdateView):
+  model = Elog
+  fields = ['name', 'distance_miles', 'length_of_time', 'reps_laps', 'weight_pounds', 'intensity', 'pace_minutes_per_mile', 'calories_burned']
+
+class ElogDelete(DeleteView):
+  model = Elog
+  success_url = 'elogs/detail.html'
+
 def home(request):
   return render(request, 'home.html')
+
+def elogs_index(request):
+  elogs = Elog.objects.all()
+  return render(request, 'elogs/index.html', {'elogs': elogs})
+'''
+def elogs_index(request):
+  elogs = UserProfile.objects.()
+  return render(request, 'elogs/index.html', {'elogs': elogs})
+'''
+def elogs_detail(request, user_id):
+  userprofile = UserProfile.objects.get(id=user_id)
+  return render(request, 'elogs/detail.html', {'userprofile': userprofile})
+  
